e13516dc730d7ed6dd9ec5da917be186821d4fd8
venicialb <venicia.leblanc@mail.mcgill.ca>
2022-01-29 14:23:12 -0500

    committing after merging

--- data/aggregate.py
+++ data/aggregate.py
@@ -10,7 +10,7 @@ def fixed_dataset() -> pd.DataFrame:
         pd.read_csv(file, parse_dates=["firstseen", "lastseen", "day"])
         # for file in Path("path/to/folder").glob("flightlist_*.csv.gz")
         for file in
-        Path("C:/Users/mowgl/Documents/Python/MIAE_Python_Tutorial/MIAE-Python/data_set").glob("flightlist_*.csv.gz")
+        Path("C:\Users\ladee\OneDrive\Bureau\Python Projects").glob("flightlist_*.csv.gz")
     )
     return flight_list
 
