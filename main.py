ea28367b0011d5b058c02771e43f65d5be107531
venicialb <venicia.leblanc@mail.mcgill.ca>
2022-01-29 11:05:01 -0500

    This is my first command.

---
+++ main.py
@@ -0,0 +1,10 @@
+def main_wrapper():
+    print("This is the start of my Python project. This function's name is {main_wrapper.__name__}")
+
+    #Code here
+
+    print("This is the end of my Python project.")
+
+
+if __name__ == "__main__":
+    main_wrapper()
